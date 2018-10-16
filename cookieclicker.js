c5f4a06664a796f79b0a912652cc1758d536e7a1
Matthieu Brouillard <mbrouilla@gmail.com>
2018-10-16 14:14:23 +0200

    Before switching to OOP

--- cookieclicker.js
+++ cookieclicker.js
@@ -1,6 +1,7 @@
 let score = 0;
 let multiplier = 1;
 let multiplierPrice = 20;
+let autoclickvalue = 0;
 
 const clickerDiv = document.querySelector("#clic");
 const scoreDiv = document.querySelector("#affichage");
@@ -27,6 +28,14 @@ function buyMultiplier(event) {
     updateScoreUI();
 }
 
+function autoClick(){
+    if (score > 20){
+        setInterval(function(){
+            clickerDiv.click();
+        }, 1000);
+    }
+}
+
 function updateScoreUI() {
     scoreDiv.innerHTML = '<h1>Cookies : ' + score + '</h1>';
 }
@@ -35,6 +44,21 @@ function updateMultiplierUI(){
     multiplierScoreText.innerHTML = 'Buy Multiplier x' + multiplier;
     multiplierPriceText.innerHTML = "Price : " + multiplierPrice;
 }
+setInterval(function(){
+    clickerDiv.click();
+}, 1000);
 
 
+class Button {
+    constructor(show, domElement){
+        this.show = show; // BOOLEAN
+        this.domElement = domElement;
+    }
 
+    changeText(text){
+        this.domElement.innerHTML = text;
+    }
+}
+
+let scoreButton = new Button(true, scoreDiv);
+scoreButton.changeText("salut");
